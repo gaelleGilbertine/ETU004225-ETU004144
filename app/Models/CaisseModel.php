6c1792231c899fe645b2dedaf3fd437951eeec82
Tsiky Christelle <tsikychristelle4@gmail.com>
2026-06-17 10:33:13 +0300

    caisseModel

---
+++ app/Models/CaisseModel.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+class CaisseModel extends Model{
+    protected $table = 'Caisse';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['nomCaisse'];
+}
+?>
