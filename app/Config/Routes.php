90101c5cacaee5ebf9b0b82640744f7bba7eacfa
Tsiky Christelle <tsikychristelle4@gmail.com>
2026-06-17 11:02:19 +0300

    Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,58 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Quantite Produit routes
+$routes->get('/quantites', 'QuantiteProduitController::findAll');
+$routes->get('/quantites/(:num)', 'QuantiteProduitController::find/$1');
+$routes->post('/quantites', 'QuantiteProduitController::create');
+$routes->post('/quantites/(:num)', 'QuantiteProduitController::update/$1');
+$routes->post('/quantites/(:num)', 'QuantiteProduitController::delete/$1');
+
+
+
+
+
+
+
+//Produit
+$routes->get('/produits', 'ProduitController::findAll');
+$routes->get('/produits/(:num)', 'ProduitController::find/$1');
+$routes->post('/produits', 'ProduitController::create');
+$routes->post('/produits/(:num)', 'ProduitController::update/$1');
+$routes->post('/produits/(:num)', 'ProduitController::delete/$1');  
+
+
+
+
+// Caisses
+$routes->get('/caisses', 'CaisseController::findAll');
+$routes->get('/caisses/(:num)', 'CaisseController::find/$1');
+$routes->post('/caisses', 'CaisseController::create');      
+$routes->post('/caisses/(:num)', 'CaisseController::update/$1');
+$routes->post('/caisses/(:num)', 'CaisseController::delete/$1');    
+
+
+
+
+
+
+// Prix
+$routes->get('/prix', 'PrixController::findAll');
+$routes->get('/prix/(:num)', 'PrixController::find/$1');
+$routes->post('/prix', 'PrixController::create');
+$routes->post('/prix/(:num)', 'PrixController::update/$1');
+$routes->post('/prix/(:num)', 'PrixController::delete/$1');
+
+
+
+// User
+$routes->get('/users', 'UserController::findAll');
+$routes->get('/users/(:num)', 'UserController::find/$1');
+$routes->post('/users', 'UserController::create');
+$routes->post('/users/(:num)', 'UserController::update/$1');
+$routes->post('/users/(:num)', 'UserController::delete/$1');
+
+
+
+
